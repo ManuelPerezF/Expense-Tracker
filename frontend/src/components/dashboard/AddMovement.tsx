ed27dab267eb40cc6e34be690c4ed09e80a1118e
ManuelPerezF <A01741957@tec.mx>
2025-09-27 15:57:16 -0600

    Eliminar componentes de movimiento: AddMovement y RecentMovements

--- frontend/src/components/dashboard/AddMovement.tsx
+++
@@ -1,9 +0,0 @@
-export default function AddMovement() {
-    return (
-        <div> 
-            <h2>Añadir Movimiento</h2>
-            {/* Aquí iría el formulario para añadir un nuevo movimiento */}
-        </div>
-    )
-
-}
--- frontend/src/components/dashboard/RecentMovements.tsx
+++
@@ -1,8 +0,0 @@
-export default function RecentMovements() {
-    return (
-        <div>
-            <h2>Movimientos Recientes</h2>
-            {/* Aquí iría la lista de movimientos recientes */}
-        </div>
-    );
-}
